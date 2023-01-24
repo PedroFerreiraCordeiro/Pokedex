a7b532ae9b25939e472e1bc9ab96e28b50aabaf7
pcordeiro <pedro.cordeiro@hotmail.com>
2023-01-24 13:16:17 -0300

    init with javascritp and async function

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const fetchPokemon = async (pokemon) => {
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIresponse.json();
+    console.log(data);
+}
+
+fetchPokemon('25');
